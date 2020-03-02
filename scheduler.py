1ebca86b254a39846697c294eed21831a2556077
Jay <jay@Jays-MacBook-Pro.local>
2020-03-01 22:55:59 -0800

    Added reset specified task time to 0.0

--- scheduler.py
+++ scheduler.py
@@ -11,9 +11,18 @@ if __name__ == '__main__':
         for line in lines[1:]:
             tasklist.append(line.split()[0])
         task = input('What task {}: '.format(tasklist)).strip()
+        if task.split()[0] == 'Reset':
+            for line in lines:
+                if line.split()[0] == task:
+                    line = line.replace(line,'{} {}\n'.format(task.split()[1], 0.0))
+                    file.write(line)
+                else: file.write(line)
+            print("Task {} time reset to 0.0".format(task.split()[1]))
+            quit()
         times = [i.replace(':', '.') for i in input('What times: ').strip().split()]
         tot = round(abs((float(times[0].split('.')[0])+float(times[0][-3:])*100/60) - \
                     (float(times[1].split('.')[0])+float(times[1][-3:])*100/60)),2)
+
         if task in tasklist:
             for line in lines:
                 if line.split()[0] == task:
